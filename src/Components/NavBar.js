7573427a0472677790747a537a088adf7793249b
Naomi Campos <n.campos92@yahoo.com>
2021-08-25 21:15:21 -0600

    Add basic styling and spacing to NavBar components

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -1,12 +1,28 @@
-import React from 'react'
+import React, {Component} from 'react'
 import NoLoLogo from '../../src/Images/NoLoLogo.png'
+import "../CSS/NavBar.css"
 
-function NavBar() {
-  return(
-    <div>
-      <img src={NoLoLogo} />
-    </div>
-  )
+class NavBar extends Component {
+  constructor() {
+    super()
+
+  }
+
+
+  render() {
+    return(
+      <div className="NavBar">
+        <div className="navButtonsContainer">
+          <button>Apply for a loan</button>
+          <button>Selling</button>
+          <button>Buying</button>
+          <button>Agents</button>
+        </div>
+      <img src={NoLoLogo} className="logo"/>
+      </div>
+    )
+  }
+
 
 }
 
