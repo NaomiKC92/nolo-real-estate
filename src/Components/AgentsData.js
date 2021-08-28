ea38d0f4b6e3cb74dbfbbfeffa4c3712081d4d09
Naomi Campos <n.campos92@yahoo.com>
2021-08-28 15:42:51 -0600

    Create mock agent data file

--- src/Components/AgentsData.js
+++ src/Components/AgentsData.js
@@ -1,1 +1,48 @@
 
+const AgentsData = [
+
+  {
+    id:1 ,
+    name: Noel Lopez,
+    image: ,
+    moreInfo: "link",
+  },
+
+  {
+    id:2 ,
+    name: Erica Lopez,
+    image: ,
+    moreInfo: "link",
+  },
+
+  {
+    id:3 ,
+    name: Juan Galvan,
+    image: ,
+    moreInfo: ,
+  },
+
+  {
+    id:4 ,
+    name: Eva Campos,
+    image: ,
+    moreInfo: ,
+  },
+
+  {
+    id:5 ,
+    name: Bianka Campos,
+    image: ,
+    moreInfo: "link",
+  },
+
+  {
+    id:6 ,
+    name: Naomi Campos,
+    image: ,
+    moreInfo: ,
+  },
+
+]
+
+export default AgentsData
