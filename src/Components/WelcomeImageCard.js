a00cdd5716b701bcc48d2f9b878d09f5c6c9aeb0
Naomi Campos <n.campos92@yahoo.com>
2021-11-22 00:38:28 -0700

    Remove console.log

--- src/Components/WelcomeImageCard.js
+++ src/Components/WelcomeImageCard.js
@@ -2,7 +2,6 @@ import React from "react";
 import "../../src/CSS/WelcomeImageCard.css";
 
 const WelcomeImageCard = ({ image }) => {
-  console.log(image);
   return (
     <div>
       <img src={image} className="welcomeImageCard" />
