7bc355cee8fb52b929b372770e234674c2be1593
Naomi Campos <n.campos92@yahoo.com>
2021-11-24 04:21:57 -0700

    Add basic styling and MLS info to footer element

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -8,6 +8,15 @@ const Footer = () => {
     return <LinkComponent key={link.id} image={link.image} />;
   });
 
-  return <div className="footer">{socialLink}</div>;
+  return (
+    <div className="footer">
+      <div classname="license-info">
+        <h3>CA DRE# 01969175/ NMLS# 1632566</h3>
+        <p>719 5th Street,</p>
+        <p>Modesto, CA 95351</p>
+      </div>
+      <div className="link-box">{socialLink}</div>
+    </div>
+  );
 };
 export default Footer;
